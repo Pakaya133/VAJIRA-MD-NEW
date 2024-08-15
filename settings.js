266f6ad0ae02739dd4b7f2fbf2861a6e26e008b2
Pakaya133 <akashlakshan551@gmail.com>
2024-08-16 00:21:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || ' ' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=3MsgXYSK#f_5liWDvWBvNQfu0kX2_1Ufbl4zQ-Ge5npj-yXLPSvo' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
